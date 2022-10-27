25a841abb460b692f43086be4a08ea27ea871c51
duanespiritu <111330062+duanespiritu@users.noreply.github.com>
2022-10-27 19:22:23 +0800

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_lahaina-user
-add_lunch_combo omni_lahaina-userdebug
-add_lunch_combo omni_lahaina-eng
+add_lunch_combo twrp_RMX3461-user
+add_lunch_combo twrp_RMX3461-userdebug
+add_lunch_combo twrp_RMX3461-eng
