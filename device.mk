3212f5318a9dfd08464a542d12e8604ad2c8dc4a
duanespiritu <111330062+duanespiritu@users.noreply.github.com>
2022-10-27 19:25:13 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-LOCAL_PATH := device/qualcomm/lahaina
+LOCAL_PATH := device/oplus/RMX3461
 # A/B
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
