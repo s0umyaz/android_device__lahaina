aeec27b85da2c7ec6fe3b7e0ec6d4d001e588c21
duanespiritu <111330062+duanespiritu@users.noreply.github.com>
2022-10-30 05:13:54 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -17,7 +17,7 @@ AB_OTA_POSTINSTALL_CONFIG += \
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.0-impl \
     android.hardware.boot@1.0-impl.recovery \   
-    android.hardware.boot@1.0-service
+    android.hardware.boot@1.0-service \
 
 PRODUCT_PACKAGES += \
     bootctrl.lahaina \
