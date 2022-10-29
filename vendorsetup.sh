eebef081d41b4732b64d246bdfa251ca13131c1c
duanespiritu <111330062+duanespiritu@users.noreply.github.com>
2022-10-30 04:21:49 +0800

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo twrp_RMX3461-user
-add_lunch_combo twrp_RMX3461-userdebug
-add_lunch_combo twrp_RMX3461-eng
+#add_lunch_combo twrp_RMX3461-user
+#add_lunch_combo twrp_RMX3461-userdebug
+#add_lunch_combo twrp_RMX3461-eng
