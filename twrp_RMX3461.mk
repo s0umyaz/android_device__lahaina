6176ce7d6f60c637d5e28e5218c71d92bbbdfb65
Carlo Dee <109793453+cd-Crypton@users.noreply.github.com>
2022-11-05 07:26:24 +0800

    Update twrp_RMX3461.mk

--- twrp_RMX3461.mk
+++ twrp_RMX3461.mk
@@ -7,12 +7,12 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common TWRP stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
 
-# Inherit from lahaina device
+# Inherit from RMX3461 device
 $(call inherit-product, device/realme/RMX3461/device.mk)
 
 PRODUCT_DEVICE := RMX3461
