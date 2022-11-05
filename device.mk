f5e98d342400c7bf58852e5ff5acf04986f174f8
Carlo Dee <109793453+cd-Crypton@users.noreply.github.com>
2022-11-05 08:04:59 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-LOCAL_PATH := device/realme/RMX3461
+LOCAL_PATH := device/realme
 # A/B
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
