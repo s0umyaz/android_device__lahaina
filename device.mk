13c6ecbb5bed1e468ae280abdcee11cd71cbe79a
Carlo Dee <109793453+cd-Crypton@users.noreply.github.com>
2022-11-09 11:35:49 +0800

    Added few more flags, vendor side.

--- device.mk
+++ device.mk
@@ -8,10 +8,30 @@
 LOCAL_PATH := device/realme/RMX3461
 # A/B
 AB_OTA_POSTINSTALL_CONFIG += \
-    RUN_POSTINSTALL_system=true \
-    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
-    FILESYSTEM_TYPE_system=ext4 \
-    POSTINSTALL_OPTIONAL_system=true
+    RUN_POSTINSTALL_vendor=true \
+    POSTINSTALL_PATH_vendor=vendor/bin/otapreopt_script \
+    FILESYSTEM_TYPE_vendor=ext4 \
+    POSTINSTALL_OPTIONAL_vendor=true
+
+# Additional Info
+TARGET_HAS_GENERIC_KERNEL_HEADERS := true
+RELAX_USES_LIBRARY_CHECK := true
+TARGET_ENFORCE_AB_OTA_PARTITION_LIST := true
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+#namespace definition for librecovery_updater
+SOONG_CONFIG_NAMESPACES += ufsbsg
+SOONG_CONFIG_ufsbsg += ufsframework
+SOONG_CONFIG_ufsbsg_ufsframework := bsg
+
+# VNDK
+PRODUCT_TARGET_VNDK_VERSION := 31
+
+# Set GRF/Vendor freeze properties
+BOARD_SHIPPING_API_LEVEL := 31
+BOARD_API_LEVEL := 31
+SHIPPING_API_LEVEL := 31
+PRODUCT_SHIPPING_API_LEVEL := 31
 
 # Boot control HAL
 PRODUCT_PACKAGES += \
@@ -45,6 +65,13 @@ PRODUCT_PACKAGES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
+    
+# OEM otacerts
+PRODUCT_EXTRA_RECOVERY_KEYS += \
+    $(LOCAL_PATH)/security/otacert
+
+# Enable Fuse Passthrough
+PRODUCT_PROPERTY_OVERRIDES += persist.sys.fuse.passthrough.enable=true
     
 # Copy modules for depmod
 PRODUCT_COPY_FILES += \
